abf4fb7ef02c67305707b4a0dd6df8d4fcd53451
Marko Islamovic <marko@gigrove.com>
2016-03-20 21:19:04 +0000

    changed index page, header, added css

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   
   
   def index
+    @posts = Post.all
   end
   
   def new
@@ -10,7 +11,7 @@ class PostsController < ApplicationController
   
   def create
     @post = Post.create(post_params)
-    redirect_to posts_path
+    redirect_to  posts_path
   end
   
   private
