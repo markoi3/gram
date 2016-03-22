0fd0a23e8f1d55f7a2fb85994f97607a5cbaa912
Marko Islamovic <marko@gigrove.com>
2016-03-22 21:07:17 +0000

    made comments and index prettier

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,17 +3,19 @@ class CommentsController < ApplicationController
 
 before_action :set_post
 
-def create  
-  @comment = @post.comments.build(comment_params)
-  @comment.user_id = current_user.id
+def create
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
 
-  if @comment.save
-    flash[:success] = "You commented the hell out of that post!"
-    redirect_to :back
-  else
-    flash[:alert] = "Check the comment form, something went horribly wrong."
-    render root_path
-  end
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    else
+      flash[:alert] = "Check the comment form, something went wrong."
+      render root_path
+    end
 end
 
 def destroy  
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
     validates :user_id, presence: true 
     belongs_to :user
-     
+     validates :caption, presence: false, length: { minimum: 0, maximum: 300 }
     has_many :comments, dependent: :destroy
     validates :image, presence: true
   has_attached_file :image, styles: { :medium => "640x" }
