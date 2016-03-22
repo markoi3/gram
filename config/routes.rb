f4709826cd1ecca7d722aff5839c6a45e7fed81a
Marko Islamovic <marko@gigrove.com>
2016-03-22 18:32:47 +0000

    added username field and validation for it in user.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+  devise_for :users, :controllers => { registrations: 'registrations' }
   get 'posts/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
---
+++ db/migrate/20160322180713_add_user_name_to_users.rb
@@ -0,0 +1,7 @@
+class AddUserNameToUsers < ActiveRecord::Migration
+  def change
+    add_index :users, :user_name, unique: true 
+    add_column :users, :user_name, :string
+    
+  end
+end
